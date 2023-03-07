5451d77176b74775e73cc305318bde119302850c
Garvit Magoo <garvit.magoo@gmail.com>
2023-03-07 11:28:05 +0530

    New Redux saga code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import UsersList from "./components/UsersList";
+import NewUserForm from "./components/NewUserForm";
+import { createUserRequest, deleteUserRequest, getUsersRequest, userError } from "./actions/users";
+import { Alert } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.props.getUsersRequest();
+  }
+
+  handleSubmit = (firstName, lastName) => {
+    console.log(firstName, lastName);
+    this.props.createUserRequest({ firstName, lastName });
+  };
+
+  handleDeleteUser = (id) => {
+    this.props.deleteUserRequest({ id });
+  };
+
+  handleCloseAlert = () => {
+    this.props.userError({error: ''})
+  }
+
+  render() {
+    const users = this.props.users;
+    return (
+      <div style={{ margin: "0 auto", padding: "20px", maxWidth: "600px" }}>
+        <Alert color="danger" isOpen={!!this.props.users.error} toggle={this.handleCloseAlert} >
+        {this.props.users.error}
+        </Alert>
+        <NewUserForm onSubmit={this.handleSubmit} />
+        <UsersList users={users.items} onDeleteUser={this.handleDeleteUser} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(({ users }) => ({ users }), {
+  getUsersRequest,
+  createUserRequest,
+  deleteUserRequest,
+  userError
+})(App);
